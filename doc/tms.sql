8af490ac2db1ac78a1f43da5c6b366af9a5f0c82
shaisxx <shihu2010@foxamil.com>
2017-03-09 18:49:44 +0800

    submit init sql

---
+++ doc/tms.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost-root
+Source Server Version : 50087
+Source Host           : localhost:3306
+Source Database       : tms
+
+Target Server Type    : MYSQL
+Target Server Version : 50087
+File Encoding         : 65001
+
+Date: 2017-03-09 18:42:06
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
